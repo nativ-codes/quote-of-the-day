e5af62426d7c98055c6b75378890d9df504dc5ad
Andrew <iAndrew3@protonmail.com>
2024-03-01 23:59:31 +0200

    updated server

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-const express = require('express')
-const fs = require('node:fs');
-const app = express()
-const port = 3000
+const express = require("express");
+const fs = require("node:fs");
+const quotes = require("./quotes.json");
+var cors = require("cors");
+const app = express();
+const port = 3000;
 
-const backgroundsPath = './backgrounds';
-const getRandomFromList = list = list[Math.floor(Math.random() * list.length)];
+// #quotes #motivation #poetry #lifequotes #rulesoflife #inspirationalquotes #inspirational #dailyquote
 
-app.get('/generate', (req, res) => {
+app.use(cors());
+
+const backgroundsPath = "./backgrounds";
+const getRandomFromList = (list) =>
+	list[Math.floor(Math.random() * list.length)];
+
+app.get("/generate", (res) => {
 	const backgrounds = fs.readdirSync(backgroundsPath);
 	const randomBackground = getRandomFromList(backgrounds);
-	const content = `Content - ${randomBackground}`;
+	const randomQuote = getRandomFromList(quotes);
+	const content = `${randomBackground}:${randomQuote.quote}\n`;
 
-	fs.appendFile('/generated.txt', content, err => {
+	fs.appendFile("generated.txt", content, (err) => {
 		if (err) {
-		  console.error(err);
+			console.error(err);
 		} else {
 			res.json({
-				imgUrl: randomBackground,
-				quote: '',
-				author: ''
+				...randomQuote,
+				day: "2 March 2024",
+				imgUrl: `${backgroundsPath}/${randomBackground}`,
 			});
 		}
 	});
-})
+});
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
