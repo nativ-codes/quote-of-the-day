0f155cf90aa84dedebda9be6b40470f27aefcf35
Andrew <iAndrew3@protonmail.com>
2024-03-02 21:17:38 +0200

    updated server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const quotes = require("./quotes.json");
 var cors = require("cors");
 const app = express();
 const port = 3000;
+const isDebugMode = true;
 
 // #quotes #motivation #poetry #lifequotes #rulesoflife #inspirationalquotes #inspirational #dailyquote
 
@@ -13,23 +14,27 @@ const backgroundsPath = "./backgrounds";
 const getRandomFromList = (list) =>
 	list[Math.floor(Math.random() * list.length)];
 
-app.get("/generate", (res) => {
+app.get("/generate", (req, res) => {
 	const backgrounds = fs.readdirSync(backgroundsPath);
 	const randomBackground = getRandomFromList(backgrounds);
 	const randomQuote = getRandomFromList(quotes);
-	const content = `${randomBackground}:${randomQuote.quote}\n`;
+	const content = `${randomBackground}:${randomQuote.author}:${randomQuote.quote}\n`;
 
-	fs.appendFile("generated.txt", content, (err) => {
-		if (err) {
-			console.error(err);
-		} else {
-			res.json({
-				...randomQuote,
-				day: "2 March 2024",
-				imgUrl: `${backgroundsPath}/${randomBackground}`,
-			});
+	fs.appendFile(
+		`generated${isDebugMode ? ".debug" : ""}.txt`,
+		content,
+		(err) => {
+			if (err) {
+				console.error(err);
+			} else {
+				res.json({
+					...randomQuote,
+					day: "2 March 2024",
+					imgUrl: `${backgroundsPath}/${randomBackground}`,
+				});
+			}
 		}
-	});
+	);
 });
 
 app.listen(port, () => {
