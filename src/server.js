7156db744d3f01caef41a3d409530860ec1625a9
Andrew <iAndrew3@protonmail.com>
2024-03-17 18:41:09 +0200

    updated pics and server

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const fs = require("node:fs");
 const path = require("path");
 const quotes = require("./quotes.json");
+const hashtags = require("./hashtags.json");
 const cors = require("cors");
 
 const app = express();
 const port = 3000;
-const isDebugMode = false;
+const isDebugMode = true;
+const generatedQuotesFile = `generated${isDebugMode ? ".debug" : ""}.txt`;
 
 app.use(cors());
 app.use(express.static("../public"));
@@ -27,33 +29,83 @@ app.get("/rename-raw", () => {
 		const extension = fileName.split(".").slice(-1)[0];
 		fs.rename(
 			`${rawBackgroundsPath}/${fileName}`,
-			`${rawBackgroundsPath}/${getRandomString()}.${extension}`
+			`${rawBackgroundsPath}/${getRandomString()}.${extension}`,
+			() => {}
 		);
 	});
 });
 
-app.get("/generate", (req, res) => {
-	const day = "4 March 2024";
+function getNewQuote(threshold = 50) {
 	const backgrounds = fs.readdirSync(backgroundsPath);
-	const randomBackground = getRandomFromList(backgrounds);
-	const randomQuote = getRandomFromList(quotes);
-	const content = `${day}:${randomBackground}:${randomQuote.author}:${randomQuote.quote}\n`;
-
-	fs.appendFile(
-		`generated${isDebugMode ? ".debug" : ""}.txt`,
-		content,
-		(err) => {
-			if (err) {
-				console.error(err);
-			} else {
-				res.json({
-					day,
-					...randomQuote,
-					imgUrl: `backgrounds/${randomBackground}`,
-				});
-			}
-		}
+	const generatedQuotesFileContent = fs.readFileSync(
+		generatedQuotesFile,
+		"UTF-8"
 	);
+	const generatedQuotesLines = generatedQuotesFileContent.split(/\r?\n/);
+
+	let counter = 0;
+
+	function tryGetUniqueQuote() {
+		const randomBackground = getRandomFromList(backgrounds);
+		const randomQuote = getRandomFromList(quotes);
+
+		if (counter < threshold) {
+			const isQuoteUnique = !generatedQuotesLines.some((line) => {
+				const [date, picture, author, quote] = line.split("::");
+
+				return (
+					randomQuote.quote === quote || randomQuote.quote === picture
+				);
+			});
+
+			if (isQuoteUnique) {
+				return {
+					...randomQuote,
+					background: randomBackground,
+				};
+			} else {
+				counter++;
+				return tryGetUniqueQuote();
+			}
+		} else {
+			throw "Unique quotes not found, try increasing the threshold.";
+		}
+	}
+
+	return tryGetUniqueQuote();
+}
+
+function getHashtags(author) {
+	const authorHashtag = `#${author.toLowerCase().split(" ").join("")}`;
+	const newHashtags = [authorHashtag];
+
+	while (newHashtags.length <= 7) {
+		const newHashtag = getRandomFromList(hashtags);
+		!newHashtags.includes(newHashtag) && newHashtags.push(newHashtag);
+	}
+
+	return newHashtags.join(", ");
+}
+
+app.get("/generate", (req, res) => {
+	const day = "17 March 2024";
+	const { background, author, quote } = getNewQuote();
+	const content = `${day}::${background}::${author}::${quote}\n`;
+	const hashtags = getHashtags(author);
+
+	fs.appendFile(generatedQuotesFile, content, (err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			res.json({
+				day,
+				quote,
+				author,
+				hashtags,
+				imgUrl: `backgrounds/${background}`,
+			});
+		}
+	});
 });
 
 app.listen(port, () => {
